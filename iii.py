52c6852dc14118e0491630f01fa0bdc56ad9c347
Liudmyla <luda.skopenko80@gmail.com>
2024-04-13 00:52:38 +0300

    added join

--- iii.py
+++ iii.py
@@ -4,17 +4,8 @@ from tabulate import tabulate
 
 
 cars = ['BMW', 'Tesla', 'Toyota', 'Mercedes',]
-cars_copy = cars.copy()
-cars_copy_2 = list(cars)
-cars_copy_3 = cars[:]
-
-print(tabulate(cars, headers=['1', '2', '3', '4', '5', '6', '7', '8', '9'], tablefmt='grid' ))
-print(cars_copy_2)
-print( cars_copy_3)
-print(len( cars))
+string = ', '.join(cars)
 
 
 
-
-
-
+print(string)
