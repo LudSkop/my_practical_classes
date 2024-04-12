718b4acb40aab7f42c6be5678349ba864c011f73
Liudmyla <luda.skopenko80@gmail.com>
2024-04-13 01:22:24 +0300

    added list

--- iii.py
+++ iii.py
@@ -4,12 +4,14 @@ from tabulate import tabulate
 
 
 cars = ['BMW', 'Tesla', 'Toyota', 'Mercedes',]
-string = ', '.join(cars)
+
+test = [45, 78, None, True, 'alisa',]
+
+appear =[cars, 'feature', test] 
 
 
+print(appear)
+print(appear[2])
+print(appear[1])
+print(appear[0])
 
-
-print(Fore.LIGHTGREEN_EX + string)
-print(type(string))
-print(cars.index('Mercedes'))
-print(cars.index('BMW'))
