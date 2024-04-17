375ecaf9c252eb171375067d436cbb9e357026ac
Liudmyla <luda.skopenko80@gmail.com>
2024-04-18 01:57:38 +0300

    create func

--- iii.py
+++ iii.py
@@ -1,21 +1,27 @@
-def lookup_key(data, value):
-    result = []
-    for key, el in data.items():
-        print(el)
-        if el == value:
-            result.append(key)
-    return print(result)
-            
-    
-    
-        
-            
-if __name__ == "__main__":
-    data = {
-        '1': 1,
-        '2': 2,
-        '3': 1,
+from tabulate import tabulate
+import sys
 
-    }  
-    val = 2
-    lookup_key(data, val)
+def split_list(grade):
+    less_avarage = []
+    more_avarage = []
+    count = 0
+    colvo = len(grade)
+    if  not grade:
+        return [], []
+    for element in grade :
+        count += int(element)
+    mid_avarage = count / colvo
+    for element in grade:
+        if int(element) <= mid_avarage:
+            less_avarage.append(element)
+        else:
+            more_avarage.append(element)
+    return less_avarage, more_avarage,
+        
+    
+    
+if __name__ == "__main__":
+    data = {3, 5, 6, 78, 90,}
+    print(split_list(sys.argv[1:]))
+    
+    
