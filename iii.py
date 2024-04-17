f823f9049daed1c4bcc9642db46a2a485e29f2a2
Liudmyla <luda.skopenko80@gmail.com>
2024-04-17 22:46:13 +0300

    added sys

--- iii.py
+++ iii.py
@@ -1,9 +1,33 @@
-import os
-from pathlib import *
+import sys
 
 
-current_dir = Path.cwd()
-print(current_dir)
-home_dir = Path.home()
-print(home_dir)
-print(os.getcwd)
+def get_grade(key):
+    k ={
+    "F":1,
+    "FX":2,
+    "E":3,
+    "D":3,
+    "C":4,
+    "B":5,
+    "A":5
+    } 
+    
+    return k.get(key)
+
+
+def get_description(key):
+    k ={
+        'F':'Unsatisfactorily',
+        'FX':'Unsatisfactorily',
+        'E':'Enough',
+        'D':'Satisfactorily',
+        'C':'Good',
+        'B':'Very good',
+        "A":"Perfectly"
+}
+    
+    return k.get(key)
+        
+if __name__ == "__main__":
+    print(get_grade(sys.argv[1]))
+
