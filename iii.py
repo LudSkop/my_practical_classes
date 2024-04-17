3fb488d316bccda5dd30bd43d2730a87d3174eb4
Liudmyla <luda.skopenko80@gmail.com>
2024-04-18 01:18:15 +0300

    create lookup_key

--- iii.py
+++ iii.py
@@ -1,34 +1,21 @@
-import sys
-
-
-def get_grade(key):
-    k ={
-    "F":1,
-    "FX":2,
-    "E":3,
-    "D":3,
-    "C":4,
-    "B":5,
-    "A":5
-    } 
+def lookup_key(data, value):
+    result = []
+    for key, el in data.items():
+        print(el)
+        if el == value:
+            result.append(key)
+    return print(result)
+            
     
-    return k.get(key)
-
-
-def get_description(key):
-    k ={
-        'F':'Unsatisfactorily',
-        'FX':'Unsatisfactorily',
-        'E':'Enough',
-        'D':'Satisfactorily',
-        'C':'Good',
-        'B':'Very good',
-        "A":"Perfectly"
-}
     
-    return k.get(key)
         
+            
 if __name__ == "__main__":
-    print(get_grade(sys.argv[4]))
-    print(get_description(sys.argv[3]))
+    data = {
+        '1': 1,
+        '2': 2,
+        '3': 1,
 
+    }  
+    val = 2
+    lookup_key(data, val)
